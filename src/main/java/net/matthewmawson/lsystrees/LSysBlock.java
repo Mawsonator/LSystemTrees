c9ff79930f28e3eaa24a75208b9a152b50ee0e66
Mawsonator <mawson1998@icloud.com>
2021-04-01 23:38:46 +0100

    Working Turtle creating Wooden Tree

--- src/main/java/net/matthewmawson/lsystrees/LSysBlock.java
+++ src/main/java/net/matthewmawson/lsystrees/LSysBlock.java
@@ -2,11 +2,10 @@ package net.matthewmawson.lsystrees;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.Material;
+import net.minecraft.block.*;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.EntityData;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -17,10 +16,16 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
+import net.minecraft.util.Pair;
+import net.minecraft.util.math.*;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Stack;
 
 
 public class LSysBlock extends Block {
@@ -34,7 +39,8 @@ public class LSysBlock extends Block {
     @Override
     public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
 
-        LSysTurtle turtle = new LSysTurtle();
+        LSysTurtle turtle = new LSysTurtle(new Vec3d( (double)pos.getX(), (double)pos.getY(), (double)pos.getZ()));
+
 
 
         // Interpret an L-System string and display it in blocks
@@ -50,11 +56,169 @@ public class LSysBlock extends Block {
 
         //MinecraftClient.getInstance().particleManager.addParticle(ParticleTypes.EXPLOSION, pos.getX());
 
+        // Stack to store the states of the turtle
+        Stack<Pair<Vec3d,double[][]>> turtleStateStack = new Stack<Pair<Vec3d,double[][]>>();
+
         String axiumS = "F+F-F-FF+F+F-F+";
         String axiumB = "F+ F-F-FF+ F+ F-F+ F+ F-F-FF+ F+ F-F-F+ F-F-FF+ F+ F-F-F+ F-F-FF+ F+ F-FF+ F-F-FF+ F+ F-F+ F+ F-F-FF+ F+ F-F+ F+ F-F-FF+ F+ F-F-F+ F-F-FF+ F+ F-F+ F+ F-F-FF+ F+ F-F+ F+ F-F-FF+ F+ F-F-F+ F-F-FF+ F+ F-F-F+ F-F-FF+ F+ F-FF+ F-F-FF+ F+ F-F+ F+ F-F-FF+ F+ F-F+ F+ F-F-FF+ F+ F-F-F+ F-F-FF+ F+ F-F+ F+ F-F-FF+ F+ F-F+ F+ F-F-FF+ F+ F-F-F+ F-F-FF+ F+ F-F-F+ F-F-FF+ F+ F-FF+ F-F-FF+ F+ F-F+ F+ F-F-FF+ F+ F-F+ F+ F-F-FF+ F+ F-F-F+ F-F-FF+ F+ F-F+ F+ F-F-FF+ F+ F-F+ F+ F-F-FF+ F+ F-F-F+ F-F-FF+ F+ F-F-F+ F-F-FF+ F+ F-FF+ F-F-FF+ F+ F-F+ F+ F-F-FF+ F+ F-F+ F+ F-F-FF+ F+ F-F-F+ F-F-FF+ F+ F-F";
         String axiumD = "FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FFFF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF-FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FFFF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FFFF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF-FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FFFF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FFFF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF-FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FFFF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FFFF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF-FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FFFF+F-F+F+FFFF+F-F+F+FF+FF+F-F+F+FF-FF+F-F+F+FF+FF+F-F+F+FF+FF+F-F+F+FFFF+F-F+F+FF";
         String axium = axiumB;
-        int blocksPerState = 2;
+        axium = "FF-[-F+F+F]+[+F-F-F]";         //35
+        axium = "^^FF-[-F+F+F]+[+F-F-F]FF-[-F+F+F]+[+F-F-F]-[-FF-[-F+F+F]+[+F-F-F]+FF-[-F+F+F]+[+F-F-F]+FF-[-F+F+F]+[+F-F-F]]+[+FF-[-F+F+F]+[+F-F-F]-FF-[-F+F+F]+[+F-F-F]-FF-[-F+F+F]+[+F-F-F]]";
+
+        //axium = "F+F+F+F+^F&F+F+F+F";
+        axium = "[&&][FFF[&&][&FFF]////[&&][&FFF]////[&&]&FFF]////[&&][FFF[&&J][&FFF]////[&&][&FFF]////[&&]&FFF]////[&&]FFF[&&J][&FFF]////[&&][&FFF]////[&&]&FFF";
+
+        /*int n = 7;
+        String rule = "A";
+        String p1 = "A-[&FLA]/////[&FLA]///////[&FLA]";
+        String p2 = "F-S/////F";
+        String p3 = "S-F";
+        String p4 = "[^^{-f+f+f-|-f+f+f}]";*/
+
+
+        /*int n = 4;              //https://www.sidefx.com/docs/houdini/nodes/sop/lsystem.html
+        String rule = "FFFA";
+        String p1 = "A-"&FFFA]////[&&][&FFFA]////[&&]&FFFA*/
+
+        int n = 7;
+        float degrees = 28;
+        String rule = "^^^FFFA";
+        String p1 = "[B]////[B]////[B]";        // First rule = A
+        String p2 = "&FFFA";                    // Second rule = B
+
+        for(int i = 0; i<n; i++){
+            axium = "";
+            while(rule.length() > 0){
+                char symbol = rule .charAt(0);
+                rule = rule.substring(1);
+
+                switch (symbol){
+                    case 'A':
+                        axium = axium + p1;
+                        break;
+                    case 'B':
+                        axium = axium + p2;
+                        break;
+                    default:
+                        axium = axium + symbol;
+                        break;
+                }
+            }
+            rule = axium;
+        }
+
+        System.out.println(axium);
+
+
+        //float degrees = 22.5f;
+        float angle = degrees*(float)Math.PI/180;
+
+        int blocksPerState = 3;
+
+        //BlockState block = Blocks.LIME_CONCRETE.getDefaultState();
+        BlockState block = Blocks.OAK_WOOD.getDefaultState();
+
+
+        while(axium.length() > 0) {
+            char symbol = axium.charAt(0);
+            axium = axium.substring(1);
+
+            System.out.println(turtle.getDirection());
+
+            switch (symbol){
+                case 'F':
+                    Vec3d oldTurtlePos = turtle.getPos();
+                    turtle.moveForward(blocksPerState);
+                    placeLine(world, block, oldTurtlePos, turtle.getPos(), turtle.getDirection());
+                    //position = placeLine(world, block, blocksPerState, direction, position);
+                    break;
+                case '+':
+                    turtle.yawLeft(angle);
+                    //direction = direction.rotateYCounterclockwise();
+                    break;
+                case '-':
+                    turtle.yawRight(angle);
+                    //direction = direction.rotateYClockwise();
+                    break;
+                case '&':
+                    turtle.pitchDown(angle);
+                    break;
+                case '^':
+                    turtle.pitchUp(angle);
+                    break;
+                case '\\':
+                    turtle.rollLeft(angle);
+                    break;
+                case '/':
+                    turtle.rollRight(angle);
+                    break;
+                case '|':
+                    turtle.turnAround();
+                    break;
+                case '[':
+                    turtleStateStack.push(new Pair<Vec3d, double[][]>(turtle.getPos(), turtle.getHLU()));
+                    break;
+                case ']':
+                    Vec3d point5 = new Vec3d(-0.5d, -0.5d, -0.5d);
+                    Pair<Vec3d, double[][]> turtleStateValues = turtleStateStack.pop();
+                    turtle = new LSysTurtle(turtleStateValues.getLeft().add(point5), turtleStateValues.getRight());
+                    break;
+
+            }
+        }
+
+        //world.setBlockState(pos.offset(Direction),state);
+        //world.setBlockState(pos.east(), Blocks.ACACIA_PLANKS.getDefaultState());
+        super.onPlaced(world, pos, state, placer, itemStack);
+    }
+
+    /* Method to place a line of blocks, currently just straight lines
+    *  Returns the position at the end of the line*/
+    private BlockPos placeLine(World world, BlockState blockState, int length, Direction direction, BlockPos pos){
+        while(length > 0){
+            world.setBlockState(pos = pos.offset(direction),blockState);
+            length--;
+        }
+        return pos;
+    }
+
+    private void placeLine(World world, BlockState block, Vec3d oldPos, Vec3d newPos, Vec3d direction){
+        // While there are more blocks to render
+        float increment = 0.5f;
+        Vec3d previousPos = oldPos;
+        while(previousPos.distanceTo(newPos) > increment){
+            //System.out.println(previousPos.distanceTo(newPos));
+            BlockPos prevBlockPos = new BlockPos(previousPos);
+            world.setBlockState(prevBlockPos, block);
+            previousPos = previousPos.add(direction.multiply(increment));
+            //System.out.println(previousPos);
+            //System.out.println(oldPos);
+            //System.out.println(newPos);
+        }
+
+        /*while(oldPos.distanceTo(turtle.getPos()) > increment){
+            BlockPos prevPos = new BlockPos(previousPos);
+            world.setBlockState(prevPos, block);
+            turtle.moveForward(increment);
+            previousPos = turtle.getPos();
+        }*/
+    }
+
+    /*@Override
+    public @Nullable BlockEntity createBlockEntity(BlockView world) {
+        return new LSysBlockBlockEntity();
+    }*/
+}
+
+
+
+
+
+/*
+Old simple code
+
+int blocksPerState = 2;
 
         BlockState block = Blocks.LIME_CONCRETE.getDefaultState();
         BlockPos position = new BlockPos(pos);
@@ -75,20 +239,4 @@ public class LSysBlock extends Block {
                     direction = direction.rotateYClockwise();
                     break;
             }
-        }
-
-        //world.setBlockState(pos.offset(Direction),state);
-        //world.setBlockState(pos.east(), Blocks.ACACIA_PLANKS.getDefaultState());
-        super.onPlaced(world, pos, state, placer, itemStack);
-    }
-
-    /* Method to place a line of blocks, currently just straight lines
-    *  Returns the position at the end of the line*/
-    private BlockPos placeLine(World world, BlockState blockState, int length, Direction direction, BlockPos pos){
-        while(length > 0){
-            world.setBlockState(pos = pos.offset(direction),blockState);
-            length--;
-        }
-        return pos;
-    }
-}
+        }*/
--- src/main/java/net/matthewmawson/lsystrees/LSysTurtle.java
+++ src/main/java/net/matthewmawson/lsystrees/LSysTurtle.java
@@ -1,60 +1,142 @@
 package net.matthewmawson.lsystrees;
 
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3i;
+import org.lwjgl.system.CallbackI;
+
+import java.util.Arrays;
+
 public class LSysTurtle {
 
-    private int posX;
-    private int posY;
+    // Vector pos
+    //private Vec3d direction;
+    private Vec3d pos;
+
+    // Turtle heading, left and up, three unit length perpendicular vectors.
+    private double[][] hlu = {
+            new double[]{1.0d, 0.0d, 0.0d},
+            new double[]{0.0d, 0.0d, 1.0d},
+            new double[]{0.0d, 1.0d, 0.0d}
+    };
+
 
     // Class to act as a the moving turtle for rendering L-Systems
     public LSysTurtle(){
 
     }
 
-
-    //MATRIX BASED PAGMAN?
-
-    public void yawLeft(){
-
+    public LSysTurtle(Vec3d blockPos){
+        // Get the centre of the block as the starting point for the turtle
+        pos = new Vec3d(blockPos.getX() + 0.5, blockPos.getY() + 0.5, blockPos.getZ() + 0.5);
+        //direction = new Vec3d(1.0d, 0.0d, 0.0d); // Start facing forwards
     }
 
-    public void yawRight(){
+    public LSysTurtle(Vec3d blockPos, double[][] directionIn){
+        // Get the centre of the block as the starting point for the turtle
+        pos = new Vec3d(blockPos.getX() + 0.5, blockPos.getY() + 0.5, blockPos.getZ() + 0.5);
+        hlu = directionIn;
 
+        double[][] ma1 = {
+                new double[]{1.0d, 2.0d, 3.0d},
+                new double[]{4.0d, 5.0d, 6.0d},
+                new double[]{7.0d, 8.0d, 9.0d}
+        };
+        double[][] ma2 = {
+                new double[]{1.0d, 0.0d, 0.0d},
+                new double[]{0.0d, 1.0d, 0.0d},
+                new double[]{0.0d, 0.0d, 1.0d}
+        };
+
+        //System.out.println(Arrays.deepToString(matrixMultiply(ma1, ma2)));
     }
 
-    public void pitchDown(){
-
+    public void moveForward(float distance){
+        Vec3d direction = new Vec3d(hlu[0][0], hlu[1][0], hlu[2][0]);
+        pos = pos.add(direction.multiply(distance));
     }
 
-    public void pitchUp(){
-
+    public void yawLeft(float angle){
+        double[][] rotMatrix = new double[][]{
+                new double[]{Math.cos(angle), Math.sin(angle), 0.0d},
+                new double[]{-Math.sin(angle), Math.cos(angle), 0.0d},
+                new double[]{0.0d, 0.0d, 1.0d}
+        };
+        hlu = matrixMultiply(hlu,rotMatrix);
     }
 
-    public void rollLeft(){
-
+    public void yawRight(float angle){
+        yawLeft(-angle);
     }
 
-    public void rollRight(){
+    public void pitchUp(float angle){
+        double[][] rotMatrix = new double[][]{
+                new double[]{Math.cos(angle), 0.0d, -Math.sin(angle)},
+                new double[]{0.0d, 1.0d, 0.0d},
+                new double[]{Math.sin(angle), 0.0d, Math.cos(angle)}
+        };
+        hlu = matrixMultiply(hlu,rotMatrix);
+    }
 
+    public void pitchDown(float angle){
+        pitchUp(-angle);
+    }
+
+    public void rollLeft(float angle){
+        double[][] rotMatrix = new double[][]{
+                new double[]{1.0d, 0.0d, 0.0d},
+                new double[]{0.0d, Math.cos(angle), -Math.sin(angle)},
+                new double[]{0.0d, Math.sin(angle), Math.cos(angle)}
+        };
+        hlu = matrixMultiply(hlu,rotMatrix);
+    }
+
+    public void rollRight(float angle){
+        rollLeft(-angle);
     }
 
     // Rotate 180deg
     public void turnAround(){
-
+        yawLeft((float)Math.PI);
     }
 
-    public int getPosX() {
-        return posX;
+    public Vec3d getPos() {
+        return pos;
     }
 
-    public int getPosY() {
-        return posY;
+    public void setPos(Vec3d posIn) {
+        pos = posIn;
     }
 
-    public void setPosX(int posX) {
-        this.posX = posX;
+    public Vec3d getDirection() {
+        return new Vec3d(hlu[0][0], hlu[1][0], hlu[2][0]);
     }
 
-    public void setPosY(int posY) {
-        this.posY = posY;
+    public double[][] getHLU() {
+        return hlu;
+    }
+
+    public void setDirection(Vec3d dirIn) {
+        //direction = dirIn;
+    }
+
+
+
+    private double[][] matrixMultiply(double[][] m1, double[][] m2){
+        double[][] output = new double[m1.length][m2[0].length];
+
+        for (int i = 0; i< output.length; i++){
+            for (int j = 0; j< output[0].length; j++){
+                // for each cell in the final matrix
+                double cellValue = 0;
+                for(int k = 0; k< m1[0].length; k++){
+                    cellValue += m1[i][k]*m2[k][j];
+                }
+                output[i][j] = cellValue;
+            }
+        }
+        return output;
+
     }
 }
